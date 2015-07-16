575fe3649e86f213f264d8638cac5295f45b3498
viestat <viestat@icloud.com>
2015-07-15 20:04:22 -0700

    Implements SongQueueView and SongQueueEntryView

--- client/views/SongQueueEntryView.js
+++ client/views/SongQueueEntryView.js
@@ -1,4 +1,11 @@
 // SongQueueEntryView.js - Defines a backbone view class for the song queue entries.
 var SongQueueEntryView = Backbone.View.extend({
+
+  tagName: 'tr',
+  template: _.template('<td>(<%= artist %>)</td><td>/<%= title %>/</td>'),
+
+  render: function () {
+    return this.$el.html(this.template(this.model.attributes));
+  }
   // your code here!
 });
